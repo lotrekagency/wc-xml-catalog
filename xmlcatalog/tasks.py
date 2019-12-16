5b13fbf13ac2be271eb24cba811a79fe87b072db
Edoardo Mazzucchielli <mazzucchielli.edoardo@gmail.com>
2019-12-16 17:45:54 +0100

    Fixed crontab for periodic tasks

--- xmlcatalog/tasks.py
+++ xmlcatalog/tasks.py
@@ -4,7 +4,7 @@ from huey import RedisHuey, crontab
 
 huey = RedisHuey('feedXML', host=REDIS_HOST)
 
-@huey.periodic_task(crontab(hour="*/7"))
+@huey.periodic_task(crontab(minute='0', hour='*/7'))
 def task():
     create_xml()
 
